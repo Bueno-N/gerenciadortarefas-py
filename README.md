1b1cc7f8ac1c8dd9117d3f6c1451e65e2e1f49eb
Nicolas Bueno <nicolas-bb@hotmail.com>
2025-11-27 01:38:29 -0300

    primeiro commit - projeto gerenciador de tarefas

---
+++ README.md
@@ -0,0 +1,22 @@
+# Gerenciador de Tarefas
+
+Projeto simples em Python para gerenciar tarefas no terminal.
+
+## Funcionalidades
+- Adicionar tarefa
+- Listar tarefas
+- Remover tarefa
+- Sair do programa
+
+## Como rodar
+1. Ative o ambiente virtual:
+   ```bash
+   source .venv/bin/activate   # Linux/macOS
+   .\.venv\Scripts\Activate.ps1  # Windows PowerShell
+
+
+Observações
+
+Projeto feito para estudos e portfólio.
+
+Não utiliza banco de dados (armazena as tarefas apenas enquanto o programa está aberto).
---
+++ gerenciador_tarefas.py
@@ -0,0 +1,61 @@
+tarefas = []
+
+def mostrar_menu():
+    print("\n=== GERENCIADOR DE TAREFAS ===")
+    print("1 - Adicionar tarefa")
+    print("2 - Listar tarefa")
+    print("3 - Remover tarefa")
+    print("4 - Sair")
+
+def adicionar_tarefa():
+    tarefa = input("Digite a tarefa: ").strip()
+    if tarefa:
+        tarefas.append(tarefa)
+        print(f"Tarefa '{tarefa}' adicionada com sucesso!")
+    else:
+        print("Nenhuma tarefa digitada.")
+
+def listar_tarefas():
+    if not tarefas:
+        print("Nenhuma tarefa cadastrada.")
+        return
+    print("\n === TAREFAS CADASTRADAS ===")
+    for i, tarefa in enumerate (tarefas, start=1):
+        print(f"{i} - {tarefa}")
+
+def remover_tarefa():
+    if not tarefas:
+        print("Nenhuma tarefa cadastrada.")
+        return
+    
+    listar_tarefas()
+    try:
+        numero = int(input("Digite o número da tarefa que deseja remover: "))
+        if 1 <= numero <= len(tarefas):
+            tarefa_removida = tarefas.pop(numero - 1)
+            print(f"Tarefa '{tarefa_removida}' removida com sucesso!" )
+        else:
+            print("Número inválido.")
+    except ValueError:
+        print("Digite um número válido.")
+
+
+        
+def main():
+    while True:
+        mostrar_menu()
+        escolha = input("Escolha: ").strip()
+        if escolha == "1":
+            adicionar_tarefa()
+        elif escolha == "2":
+            listar_tarefas()
+        elif escolha == "3":
+            remover_tarefa()
+        elif escolha == "4":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida.")
+
+if __name__ == "__main__":
+    main()
